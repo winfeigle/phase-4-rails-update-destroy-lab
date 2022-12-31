74d9e8deaf7434949690079cf738700afd65777c
Win Feigle <win.feigle@gmail.com>
2022-12-31 12:33:58 -0700

    commit tree b464fe2c25845ea63407d2da504fd00c904b489f

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,6 +18,21 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  # PATCH /plants/:id
+  def update
+    plant = Plant.find_by(id: params[:id])
+    plant.update(plant_params)
+    render json: plant, status: :accepted
+  end
+
+  # DELETE /plants/:id
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    plant.destroy
+    head :no_content
+  end
+
+
   private
 
   def plant_params
